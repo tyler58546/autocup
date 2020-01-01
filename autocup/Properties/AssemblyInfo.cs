700a4ee569fa58016a9c710853561b25a6b93f4f
Tyler Knox <tyler58546@gmail.com>
2020-01-01 10:07:17 -0800

    Update copyright notice

--- autocup/Properties/AssemblyInfo.cs
+++ autocup/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("autocup")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Tyler Knox")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
